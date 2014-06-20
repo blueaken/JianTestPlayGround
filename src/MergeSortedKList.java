554356e8f02f31479251b3b67e898a2fd481d1b7
BruceShen <blueaken@gmail.com>
2014-06-20 08:46:45 -0700

    add initial test

--- src/MergeSortedKList.java
+++ src/MergeSortedKList.java
@@ -5,9 +5,7 @@
  * Time: 12:17 上午
  */
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.*;
 
 /**
  * Definition for singly-linked list.
@@ -32,7 +30,7 @@ class ListNodeComparator implements Comparator<ListNode> {
  * Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
  */
 public class MergeSortedKList {
-    public ListNode mergeKLists(List<ListNode> lists) {
+    public static ListNode mergeKLists(List<ListNode> lists) {
         if (lists.size() == 0) return null;
         Comparator<ListNode> listNodeComparator = new ListNodeComparator();
         PriorityQueue<ListNode> priorityQueue = new PriorityQueue<ListNode>(lists.size(), listNodeComparator);
@@ -42,15 +40,44 @@ public class MergeSortedKList {
         ListNode head = new ListNode(-999);
         ListNode p = head;
 
-        while (lists.size() != 0) {
+        while (priorityQueue.size() != 0) {
             ListNode current = priorityQueue.remove();
             p.val = current.val;
+            p.next = new ListNode(-999);
             p = p.next;
             if (current.next != null){
-                priorityQueue.add(current);
+                priorityQueue.add(current.next);
             }
         }
         return head;
+    }
+
+    public static void main(String[] args){
+        //1,4,7
+        ListNode ln1 = new ListNode(1);
+        ln1.next = new ListNode(4);
+        ln1.next.next = new ListNode(7);
+
+        //2,5,8
+        ListNode ln2 = new ListNode(2);
+        ln2.next = new ListNode(5);
+        ln2.next.next = new ListNode(8);
+
+        //3,6,9
+        ListNode ln3 = new ListNode(3);
+        ln3.next = new ListNode(6);
+        ln3.next.next = new ListNode(9);
+
+        List<ListNode> lists = new ArrayList<ListNode>(3);
+        lists.add(ln1);
+        lists.add(ln2);
+        lists.add(ln3);
+
+        //1,2,3,4,5,6,7,8,9
+        ListNode result = mergeKLists(lists);
+        while (result!=null){
+            System.out.print(result.val);
+        }
     }
 
 }
