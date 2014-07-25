0efbc531e576b898f2ca4706e63530b2bc257d1a
BruceShen <blueaken@gmail.com>
2014-07-24 23:32:13 -0700

    Interval class

---
+++ src/type/Interval.java
@@ -0,0 +1,12 @@
+package type;
+
+/**
+ * Author: blueaken
+ * Date: 7/24/14 11:31 下午
+ */
+public class Interval {
+    int start;
+    int end;
+    Interval() { start = 0; end = 0; }
+    Interval(int s, int e) { start = s; end = e; }
+}
