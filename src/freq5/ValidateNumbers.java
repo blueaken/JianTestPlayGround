4c06502ce31d16a9fc528a428eae39de28409dbb
BruceShen <blueaken@gmail.com>
2014-07-28 20:59:51 -0700

    ValidateNumbers - 我的奋斗的解法

--- src/freq5/ValidateNumbers.java
+++ src/freq5/ValidateNumbers.java
@@ -5,37 +5,45 @@ package freq5;
  * Date: 7/27/14 11:07 下午
  */
 public class ValidateNumbers {
-    public static boolean isNumber(String str) {
-        if (str == null) return false;
+    //my first try
+//    public static boolean isNumber(String str) {
+//        if (str == null) return false;
+//
+//        //remove begin and end white spaces
+//        str = str.trim();
+//
+//        if (str.length()==0) return false;
+//
+//        try{
+//            Long.parseLong(str);
+//            return true;
+//        } catch (Exception e1){
+//            try{
+//                Float.parseFloat(str);
+//                return true;
+//            } catch (Exception e2){
+//                try{
+//                    Double.parseDouble(str);
+//                    return true;
+//                } catch (Exception e3){
+//                    return false;
+//                }
+//            }
+//        }
+//
+//    }
 
-        //remove begin and end white spaces
-        str = str.trim();
-
-        if (str.length()==0) return false;
-
-        //process sign
-        char firstChar = str.charAt(0);
-        boolean isNeg = false;
-        if (firstChar == '+'){
-            str = str.substring(1);
+    //fight for your dreams 的解法，直接用正则表达式
+    public static boolean isNumber(String s) {
+        if(s.trim().isEmpty()){
+            return false;
         }
-        if (firstChar == '-'){
-            str = str.substring(1);
-            isNeg = true;
+        String regex = "[-+]?(\\d+\\.?|\\.\\d+)\\d*(e[-+]?\\d+)?";
+        if(s.trim().matches(regex)){
+            return true;
+        }else{
+            return false;
         }
-
-        try{
-            long temp = Long.parseLong(str);
-        } catch (Exception e1){
-            try{
-                double temp = Double.parseDouble(str);
-                return true;
-            } catch (Exception e2){
-                return false;
-            }
-        }
-
-        return true;
     }
 
     public static void main(String[] args){
@@ -45,7 +53,8 @@ public class ValidateNumbers {
 //        String test = "- 321rrr";
 //        String test = " 2e10";
 //        String test = "3.65f";
-        String test = "0xff";
+//        String test = "0xff";
+        String test = "959440.94f";
 
         boolean value = isNumber(test);
         System.out.println("String " + test + " is a number: " + value);
