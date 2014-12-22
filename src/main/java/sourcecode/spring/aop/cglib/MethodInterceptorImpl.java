0e333e751b56c1f63289a566dcaded3886ad4817
jianshen <jianshen@ebay.com>
2014-12-21 23:11:39 -0800

    Morgan Stanley interview prep

--- src/main/java/sourcecode/spring/aop/cglib/MethodInterceptorImpl.java
+++ src/main/java/sourcecode/spring/aop/cglib/MethodInterceptorImpl.java
@@ -1,18 +1,18 @@
 package sourcecode.spring.aop.cglib;
 
-import net.sf.cglib.proxy.MethodInterceptor;
-import net.sf.cglib.proxy.MethodProxy;
+//import net.sf.cglib.proxy.MethodInterceptor;
+//import net.sf.cglib.proxy.MethodProxy;
 
 import java.lang.reflect.Method;
 
 /**
  * @author jianshen
  */
-public class MethodInterceptorImpl implements MethodInterceptor {
-    public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable{
-        System.out.println(method);
-        proxy.invokeSuper(obj, args);
-
-        return null;
-    }
-}
+//public class MethodInterceptorImpl implements MethodInterceptor {
+//    public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable{
+//        System.out.println(method);
+//        proxy.invokeSuper(obj, args);
+//
+//        return null;
+//    }
+//}
--- src/main/java/sourcecode/spring/aop/cglib/Test.java
+++ src/main/java/sourcecode/spring/aop/cglib/Test.java
@@ -1,18 +1,18 @@
 package sourcecode.spring.aop.cglib;
 
-import net.sf.cglib.proxy.Enhancer;
+//import net.sf.cglib.proxy.Enhancer;
 
 /**
  * @author jianshen
  */
 public class Test {
-    public static void main(String[] args){
-        Enhancer enhancer = new Enhancer();
-        enhancer.setSuperclass(MyAOP.class);
-        //设置callback方法实现类
-        enhancer.setCallback(new MethodInterceptorImpl());
-        //实例化已经添加callback方法的MyAOP实例
-        MyAOP myAOP = (MyAOP)enhancer.create();
-        myAOP.print("test");
-    }
+//    public static void main(String[] args){
+//        Enhancer enhancer = new Enhancer();
+//        enhancer.setSuperclass(MyAOP.class);
+//        //设置callback方法实现类
+//        enhancer.setCallback(new MethodInterceptorImpl());
+//        //实例化已经添加callback方法的MyAOP实例
+//        MyAOP myAOP = (MyAOP)enhancer.create();
+//        myAOP.print("test");
+//    }
 }
