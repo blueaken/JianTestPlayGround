b9f3d4c5b1f9b5d7390cd24377f7c9a784cce092
jianshen <jianshen@ebay.com>
2015-01-11 19:33:47 -0800

    DeadLockDemo - BankAccount

---
+++ src/main/java/interviewprep/morganstanley/BankAccount_DeadLockDemo.java
@@ -0,0 +1,31 @@
+package interviewprep.morganstanley;
+
+/**
+ * Created by jianshen on 1/11/15.
+ */
+/*
+ * In the Bank Account example below, if 2 transfer operations between 2 accounts occur at the same time:
+ * transfer(a, b, 100.0) and transfer(b, a, 50.0). Deadlock will happen they are trying to acquire the
+ * resources in the reverse order.
+ */
+public class BankAccount_DeadLockDemo {
+    private double balance;
+    private int accountId;
+
+    public void deposit(double amount){
+        balance += amount;
+    }
+
+    public void withdraw(double amount){
+        balance -= amount;
+    }
+
+    public void transfer (BankAccount_DeadLockDemo from, BankAccount_DeadLockDemo to, double amount){
+        synchronized (from){
+            synchronized (to){
+                from.withdraw(amount);
+                to.deposit(amount);
+            }
+        }
+    }
+}
