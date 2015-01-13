fcba7646c636439f216700b99ccd9678a87b3172
jianshen <jianshen@ebay.com>
2015-01-12 16:52:07 -0800

    Decorator pattern sample code.

---
+++ src/main/java/interviewprep/morganstanley/decorator_pattern/Coffee.java
@@ -0,0 +1,9 @@
+package interviewprep.morganstanley.decorator_pattern;
+
+/**
+ * @author jianshen
+ */
+public abstract class Coffee {
+    public abstract double getCost();
+    public abstract String getIngredients();
+}
---
+++ src/main/java/interviewprep/morganstanley/decorator_pattern/SimpleCoffee.java
@@ -0,0 +1,16 @@
+package interviewprep.morganstanley.decorator_pattern;
+
+/**
+ * @author jianshen
+ */
+public class SimpleCoffee extends Coffee {
+
+    public double getCost(){
+        return 1.0;
+    }
+
+    public String getIngredients(){
+        return "Coffee";
+    }
+
+}
