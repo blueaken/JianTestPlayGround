889fed77a1f8432cbb6ccc1ad0a193c770ac0dc6
Jian Shen <jian.shen@nike.com>
2015-06-27 01:05:28 -0700

    ＃155 Min STACK - add solution with one stack and test code.

---
+++ src/main/java/leetcode/easy/minstack/MinStackTest.java
@@ -0,0 +1,19 @@
+package leetcode.easy.minstack;
+
+/**
+ * Created by jshe18 on 6/27/15.
+ */
+public class MinStackTest {
+    public static void main(String[] args){
+        MinStack_WithOneStack minStack = new MinStack_WithOneStack();
+        minStack.push(2);
+        //minStack.push(2);
+
+        int min, top;
+        min = minStack.getMin();
+        System.out.println(min);
+
+        top = minStack.top();
+        System.out.println(top);
+    }
+}
---
+++ src/main/java/leetcode/easy/minstack/MinStack_WithOneStack.java
@@ -0,0 +1,46 @@
+package leetcode.easy.minstack;
+
+import java.util.Stack;
+
+/**
+ * Created by jshe18 on 6/25/15.
+ */
+public class MinStack_WithOneStack {
+
+    long min; //long is used to prevent overflow
+    Stack<Long> stack = new Stack<Long>();
+
+   public void push(int x){
+       if (stack.isEmpty()){
+           min = x;
+       }
+
+       stack.push(x - min); //could be negative if min needs change
+       if (x < min) {
+           min = x;
+       }
+   }
+
+   public int pop(){
+       long pop = stack.pop();
+       if (pop > 0){
+           return (int)(pop+min);
+       }else{
+           min = min-pop; //increase the min
+           return (int)min;
+       }
+   }
+
+   public int top(){
+       long peek = stack.peek();
+       if (peek > 0){
+           return (int)(peek+min);
+       }else{
+           return (int)min;
+       }
+   }
+
+   public int getMin(){
+       return (int)min;
+   }
+}
