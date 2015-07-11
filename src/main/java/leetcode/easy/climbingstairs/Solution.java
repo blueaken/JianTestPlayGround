6df158bcbf7b7f4bff8ffc226ac59e18040052d2
Jian Shen <jian.shen@nike.com>
2015-07-11 15:38:30 -0700

    ＃70 Climbing Stairs

---
+++ src/main/java/leetcode/easy/climbingstairs/Solution.java
@@ -0,0 +1,24 @@
+package leetcode.easy.climbingstairs;
+
+/**
+ * Created by jshe18 on 7/11/15.
+ */
+public class Solution {
+    public int climbStairs(int n) {
+        if (n<3){
+            return n;
+        }
+
+        int p = 2;
+        int pp = 1;
+
+        int result = 0;
+        for (int i=3; i<=n; i++){
+            result = p + pp;
+            pp = p;
+            p = result;
+        }
+
+        return result;
+    }
+}
