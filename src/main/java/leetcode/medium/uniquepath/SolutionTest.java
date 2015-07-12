913a38b9df704cb973ecc91990d2a4dba21d7ac9
Jian Shen <jian.shen@nike.com>
2015-07-12 15:52:18 -0700

    ＃62 Unique Path, add test

---
+++ src/main/java/leetcode/medium/uniquepath/SolutionTest.java
@@ -0,0 +1,11 @@
+package leetcode.medium.uniquepath;
+
+/**
+ * Created by jshe18 on 7/12/15.
+ */
+public class SolutionTest {
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        System.out.println("The actual result is: " + solution.uniquePaths(3,3));
+    }
+}
