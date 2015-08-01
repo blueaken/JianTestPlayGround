728de8e06230d0c13669cae9c347e2f99b211edf
Jian Shen <jian.shen@nike.com>
2015-08-01 15:06:25 -0700

    #153 Find Minimum in Rotated Sorted Array - my solution is recursion and passed OJ. but admit the while loop is with less code and elegant, the downside is its performance is worse than mine :)

---
+++ src/main/java/leetcode/medium/findminrotatedsortedarray/SolutionTest.java
@@ -0,0 +1,15 @@
+package leetcode.medium.findminrotatedsortedarray;
+
+/**
+ * Created by jshe18 on 8/1/15.
+ */
+public class SolutionTest {
+    public static void main(String[] args){
+        int[] nums = {6,-3,1,2,3,4,5};//left
+//        int[] nums = {4,5,6,7,8,9,10,11,12,-2,1,2,3};//right
+        //int[] nums = {4,5,6,-2,1,2,3};//middle
+        //int[] nums = {1,2,3};
+        //System.out.println("min value from the rotated array is: "  + new Solution().findMin(nums));
+        System.out.println("min value from the rotated array is: "  + new Solution_WhileLoop().findMin(nums));
+    }
+}
