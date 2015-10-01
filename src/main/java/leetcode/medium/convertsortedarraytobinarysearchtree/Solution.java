4b6efe9825332dc6cd00eda03a913ce0bfe508d3
BruceShen <blueaken@gmail.com>
2015-10-01 15:12:40 +0800

    #108/109 Convert sorted array/list to BST - List的难点在于list不能象array那样用O（1）来读取中位值。可用每次遍历两遍的方法，但时间复杂度太高。可以用快慢指针方法实现遍历一遍就找到中位值来解决。

---
+++ src/main/java/leetcode/medium/convertsortedarraytobinarysearchtree/Solution.java
@@ -0,0 +1,33 @@
+package leetcode.medium.convertsortedarraytobinarysearchtree;
+
+import type.TreeNode;
+
+/**
+ * Author: blueaken
+ * Date: 9/30/15 9:14 AM
+ */
+public class Solution {
+    public static TreeNode sortedArrayToBST(int[] nums) {
+        if (nums==null) return null;
+        TreeNode bst = rec(nums, 0, nums.length-1);
+        return bst;
+    }
+
+    static TreeNode rec(int[] nums, int start, int end){
+        //close condition;
+        if (start>end) return null;
+
+        int mid = start + (end-start)/2;
+        TreeNode node = new TreeNode(nums[mid]);
+        node.left = rec(nums, start, mid-1);
+        node.right = rec(nums, mid+1, end);
+
+        return node;
+    }
+
+    public static void main(String[] args){
+        int[] sortedArr = {1,2,3,4,5,6,7,8,9};
+        TreeNode bst = sortedArrayToBST(sortedArr);
+    }
+
+}
