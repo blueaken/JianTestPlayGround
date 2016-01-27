1acca5f99901edf8947b0180d5813baec94ffbad
BruceShen <blueaken@gmail.com>
2016-01-26 16:28:16 -0800

    #163 Missing Ranges

---
+++ src/main/java/leetcode/medium/missingranges/Solution.java
@@ -0,0 +1,42 @@
+package leetcode.medium.missingranges;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Author: blueaken
+ * Date: 1/26/16 3:58 PM
+ */
+public class Solution {
+    public static List<String> findMissingRanges(int[] vals, int start, int end) {
+        List<String> result = new ArrayList<>();
+        int prev = start - 1;
+
+        for (int i=0; i<=vals.length; i++){
+            int cur = (i==vals.length ? (end+1) : vals[i]);
+            if (cur - prev > 1){
+                result.add(getRange(prev+1, cur-1));
+            }
+            prev = cur;
+        }
+
+        return result;
+    }
+
+    private static String getRange(int from, int to){
+        return from == to ? String.valueOf(from) : from + "->" + to;
+    }
+
+    public static void main(String[] args){
+//        int[] input = {0, 1, 3, 50, 75};
+        int[] input = {};
+        int start = 0;
+        int end = 99;
+
+        List<String> result = findMissingRanges(input, start, end);
+        for (String s : result){
+            System.out.println(s);
+        }
+    }
+
+}
---
+++ src/main/java/leetcode/medium/missingranges/readme.txt
@@ -0,0 +1,5 @@
+
+- if we could add two “artificial” elements, –1 before the first element and 100 after the last element, we could avoid
+many edge cases.
+
+- you should be able to extend the above cases not only for the range [0,99], but any arbitrary range [start, end].
