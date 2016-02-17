76839ef0c07e4cc3ad7fbf3ed6d4e5094fad36fa
BruceShen <blueaken@gmail.com>
2016-02-17 09:00:41 -0800

    #26 Implement strstr 2nd

---
+++ src/main/java/leetcode/easy/strstr/second/Solution.java
@@ -0,0 +1,47 @@
+package leetcode.easy.strstr.second;
+
+/**
+ * Author: blueaken
+ * Date: 2/16/16 1:26 PM
+ */
+public class Solution {
+    public static int strStr(String haystack, String needle) {
+        int h = haystack.length();
+        int n = needle.length();
+
+        int hind = 0;
+        while (hind <= h){
+            for (int nind = 0; ; nind++){
+                if (nind == n) return hind;
+                if (nind + hind == h) return -1;
+                if (needle.charAt(nind) != haystack.charAt(hind+nind)) break;
+            }
+            hind++;
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args){
+        String haystack = "asdfsadfsa";
+        String needle = "x";
+        System.out.println(strStr(haystack, needle));
+
+        haystack = "a";
+        needle = "a";
+        System.out.println(strStr(haystack, needle));
+
+        haystack = "aaa";
+        needle = "aaaa";
+        System.out.println(strStr(haystack, needle));
+
+        haystack = "mississippi";
+        needle = "issip";
+        System.out.println(strStr(haystack, needle));
+
+        haystack = "";
+        needle = "";
+        System.out.println(strStr(haystack, needle));
+    }
+
+}
