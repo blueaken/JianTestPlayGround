a276acb11ae6c9d6ec84ab5806a7adbab705dca9
BruceShen <blueaken@gmail.com>
2016-02-18 14:45:24 -0800

    #17 Letter Combinations of a Phone Number 2nd

---
+++ src/main/java/leetcode/medium/lettercombinationsofaphonenumber/second/Solution_Rec_Minus.java
@@ -0,0 +1,59 @@
+package leetcode.medium.lettercombinationsofaphonenumber.second;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Author: blueaken
+ * Date: 2/17/16 9:43 AM
+ */
+public class Solution_Rec_Minus {
+
+    private static final Map<Character, String> DICTIONARY = new HashMap<Character, String>(){{
+        put('2', "abc");
+        put('3', "def");
+        put('4', "ghi");
+        put('5', "jkl");
+        put('6', "mno");
+        put('7', "pqrs");
+        put('8', "tuv");
+        put('9', "wxyz");
+        put('0', "");
+    }};
+
+    private static List<String> result = new ArrayList<>();
+
+    public static List<String> letterCombinations(String digits) {
+        if (digits == null || digits.length() == 0) return result;
+
+        rec(digits, digits.length()-1);
+        return result;
+    }
+
+    private static void rec(String digits, int index){
+        if (index == -1) {
+            result.add("");
+            return;
+        }
+
+        rec(digits, index-1);
+        int size = result.size();
+        List<String> temp = new ArrayList<>();
+        for (int i=0; i<size; i++){
+            String mapString = DICTIONARY.get(digits.charAt(index));
+            for (int j=0; j<mapString.length(); j++){
+                temp.add(result.get(i) + mapString.charAt(j));
+            }
+        }
+        result = temp;
+    }
+
+    public static void main(String[] args){
+        List<String> res = letterCombinations("23");
+        for (String s : res){
+            System.out.println(s);
+        }
+    }
+}
---
+++ src/main/java/leetcode/medium/lettercombinationsofaphonenumber/second/Solution_Rec_Plus.java
@@ -0,0 +1,56 @@
+package leetcode.medium.lettercombinationsofaphonenumber.second;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Author: blueaken
+ * Date: 2/18/16 2:17 PM
+ */
+public class Solution_Rec_Plus {
+
+    private static final Map<Character, String> DICTIONARY = new HashMap<Character, String>(){{
+        put('2', "abc");
+        put('3', "def");
+        put('4', "ghi");
+        put('5', "jkl");
+        put('6', "mno");
+        put('7', "pqrs");
+        put('8', "tuv");
+        put('9', "wxyz");
+        put('0', "");
+    }};
+
+    private static List<String> result = new ArrayList<>();
+
+    public static List<String> letterCombinations(String digits) {
+        if (digits == null || digits.length() == 0) return result;
+
+        StringBuilder temp = new StringBuilder();
+        rec(digits, 0, temp);
+        return result;
+    }
+
+    private static void rec(String digits, int index, StringBuilder temp){
+        if (index == digits.length()){
+            result.add(temp.toString());
+            return;
+        }
+
+        String map = DICTIONARY.get(digits.charAt(index));
+        for (int i=0; i<map.length(); i++){
+            temp.append(map.charAt(i));
+            rec(digits, index+1, temp);
+            temp.deleteCharAt(temp.length()-1);
+        }
+    }
+
+    public static void main(String[] args){
+        List<String> res = letterCombinations("23");
+        for (String s : res){
+            System.out.println(s);
+        }
+    }
+}
