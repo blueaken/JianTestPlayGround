569faad48d704d76402f7a41cfe2c5d8f7050485
BruceShen <blueaken@gmail.com>
2016-02-27 14:58:11 -0800

    nine chapter 3 binary tree & divide conquer - inorder iterative solution template

---
+++ src/main/java/ninechapter/chapter3_binarytree_and_divideconquer/optional/inordertraversal/Solution_Iterative_template.java
@@ -0,0 +1,48 @@
+package ninechapter.chapter3_binarytree_and_divideconquer.optional.inordertraversal;
+
+import type.TreeNode;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+/**
+ * Author: blueaken
+ * Date: 2/27/16 2:17 PM
+ */
+public class Solution_Iterative_template {
+    /**
+     * @param root: The root of binary tree.
+     * @return: Inorder in ArrayList which contains node values.
+     */
+    public static ArrayList<Integer> inorderTraversal(TreeNode root) {
+        // write your code here
+        ArrayList<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode node = root;
+        while (!stack.isEmpty() || node != null) {
+            while (node != null) {
+                stack.push(node);
+                node = node.left;
+            }
+            if (!stack.isEmpty()) {
+                node = stack.pop();
+                result.add(node.val);
+                node = node.right;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        TreeNode node = new TreeNode(1);
+        node.left = new TreeNode(2);
+        node.right = new TreeNode(3);
+
+        System.out.println(inorderTraversal(node));
+    }
+}
