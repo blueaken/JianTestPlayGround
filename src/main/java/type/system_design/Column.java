fe417020b1e2200b5f425cc82d6c3d80d28c9b16
BruceShen <blueaken@gmail.com>
2016-04-03 22:29:15 -0700

    system design types

---
+++ src/main/java/type/system_design/Column.java
@@ -0,0 +1,14 @@
+package type.system_design;
+
+/**
+ * Author: blueaken
+ * Date: 3/20/16 5:50 PM
+ */
+public class Column {
+    public int key;
+    public String value;
+    public Column(int key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+}
