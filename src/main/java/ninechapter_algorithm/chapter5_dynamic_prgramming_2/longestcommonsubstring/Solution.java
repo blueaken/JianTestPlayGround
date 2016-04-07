f45c6b1559dd0e809c7a23d45bf9af41ac59086c
BruceShen <blueaken@gmail.com>
2016-04-07 10:50:49 -0700

    nine chapter 5 dynamic programming 2 - longest common substring

--- src/main/java/ninechapter_algorithm/chapter5_dynamic_prgramming_2/longestcommonsubstring/Solution.java
+++ src/main/java/ninechapter_algorithm/chapter5_dynamic_prgramming_2/longestcommonsubstring/Solution.java
@@ -1,15 +1,15 @@
-package ninechapter_algorithm.chapter5_dynamic_prgramming_2.longestcommonsubsequence;
+package ninechapter_algorithm.chapter5_dynamic_prgramming_2.longestcommonsubstring;
 
 /**
  * Author: blueaken
- * Date: 4/7/16 9:47 AM
+ * Date: 4/7/16 9:55 AM
  */
 public class Solution {
     /**
      * @param A, B: Two string.
      * @return: the length of the longest common substring.
      */
-    public static int longestCommonSubsequence(String A, String B) {
+    public static int longestCommonSubstring(String A, String B) {
         // write your code here
         if (A == null || A.length() == 0
                 || B == null || B.length() == 0) {
@@ -28,23 +28,25 @@ public class Solution {
         }
 
         //dp
-        //ref: https://www.youtube.com/watch?v=NnD96abizww
+        //https://www.youtube.com/watch?v=BysNXJHzCEs
+        int max = Integer.MIN_VALUE;
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
                 if (A.charAt(i-1) == B.charAt(j-1)) {
                     res[i][j] = res[i-1][j-1] + 1;
                 } else {
-                    res[i][j] = Math.max(res[i][j-1], res[i-1][j]);
+                    res[i][j] = 0;
                 }
+                max = Math.max(res[i][j], max);
             }
         }
 
-        return res[n][m];
+        return max;
     }
 
     public static void main(String[] args) {
         String A = "abcdaf";
-        String B = "acbcf";
-        System.out.println(longestCommonSubsequence(A, B));
+        String B = "ebcdf";
+        System.out.println(longestCommonSubstring(A, B));
     }
 }
