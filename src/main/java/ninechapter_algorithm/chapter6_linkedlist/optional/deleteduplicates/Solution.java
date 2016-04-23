3830d54d8d397d9b63f9b2cb7462de928e37ce86
BruceShen <blueaken@gmail.com>
2016-04-23 11:37:04 -0700

    nine chapter 6 linked list - remove duplicates

---
+++ src/main/java/ninechapter_algorithm/chapter6_linkedlist/optional/deleteduplicates/Solution.java
@@ -0,0 +1,46 @@
+package ninechapter_algorithm.chapter6_linkedlist.optional.deleteduplicates;
+
+import type.ListNode;
+
+/**
+ * Author: blueaken
+ * Date: 4/23/16 10:54 AM
+ */
+public class Solution {
+    /**
+     * @param ListNode head is the head of the linked list
+     * @return: ListNode head of linked list
+     */
+    public static ListNode deleteDuplicates(ListNode head) {
+        // write your code here
+        if (head == null) {
+            return head;
+        }
+
+        ListNode node = head;
+        while (node.next != null) {
+            if (node.val == node.next.val) {
+                node.next = node.next.next;
+            } else {
+                node = node.next;
+            }
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        //tc 1
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(1);
+        head.next.next = new ListNode(2);
+        head.next.next.next = new ListNode(3);
+        head.next.next.next.next = new ListNode(3);
+
+        //tc 2
+//        ListNode head = new ListNode(1);
+//        head.next = new ListNode(1);
+//        head.next.next = new ListNode(2);
+
+        deleteDuplicates(head).print();
+    }
+}
