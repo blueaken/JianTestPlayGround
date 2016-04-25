749f0377a7e5ef24f9fabfb988f9e219738b61fe
BruceShen <blueaken@gmail.com>
2016-04-25 15:34:43 -0700

    add print() method into ListNode / RandomListNode type

--- src/main/java/type/ListNode.java
+++ src/main/java/type/ListNode.java
@@ -9,5 +9,16 @@ package type;
     public ListNode(int x) {
           val = x;
           next = null;
-      }
+    }
+
+    public void print() {
+        ListNode node = this;
+        while (node != null) {
+            System.out.print(node.val + "->");
+            node = node.next;
+        }
+        System.out.print("null");
+        System.out.println();
+    }
+
   }
--- src/main/java/type/RandomListNode.java
+++ src/main/java/type/RandomListNode.java
@@ -7,4 +7,14 @@ public class RandomListNode {
     public int label;
     public RandomListNode next, random;
     public RandomListNode(int x) { this.label = x; }
+
+    public void print() {
+        RandomListNode node = this;
+        while (node != null) {
+            System.out.print(node.label + "->");
+            node = node.next;
+        }
+        System.out.print("null");
+        System.out.println();
+    }
 }
