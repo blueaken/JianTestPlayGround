de0d46f9941fdf457db4e47396eec1ab31dec9d2
BruceShen <blueaken@gmail.com>
2016-05-09 10:24:11 -0700

    nine chapter 9 graph and search - clone graph - dfs

---
+++ src/main/java/ninechapter_algorithm/chapter9_graphic/clonegraph/Solution_DFS.java
@@ -0,0 +1,55 @@
+package ninechapter_algorithm.chapter9_graphic.clonegraph;
+
+import type.UndirectedGraphNode;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Author: blueaken
+ * Date: 5/9/16 10:22
+ */
+public class Solution_DFS {
+    /**
+     * @param node: A undirected graph node
+     * @return: A undirected graph node
+     */
+    public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+        // write your code here
+        if (node == null) {
+            return null;
+        }
+
+        Map<UndirectedGraphNode, UndirectedGraphNode> map = new HashMap<>();
+        Set<UndirectedGraphNode> visited = new HashSet<>();
+        visited.add(node);
+
+        //1. find all nodes - dfs
+        dfs(node, visited);
+
+        //2. copy nodes
+        for (UndirectedGraphNode n : visited) {
+            map.put(n, new UndirectedGraphNode(n.label));
+        }
+
+        //3. copy edges
+        for (UndirectedGraphNode n : visited) {
+            for (UndirectedGraphNode neighbor : n.neighbors) {
+                map.get(n).neighbors.add(map.get(neighbor));
+            }
+        }
+
+        return map.get(node);
+    }
+
+    private void dfs(UndirectedGraphNode node, Set<UndirectedGraphNode> visited) {
+        for (UndirectedGraphNode n : node.neighbors) {
+            if (!visited.contains(n)) {
+                visited.add(n);
+                dfs(n, visited);
+            }
+        }
+    }
+}
