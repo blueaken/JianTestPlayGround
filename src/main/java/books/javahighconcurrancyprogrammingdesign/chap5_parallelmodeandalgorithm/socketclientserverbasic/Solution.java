83586444f0255f906001c809eb4fdac51039bb1a
BruceShen <blueaken@gmail.com>
2016-05-25 15:19:26 -0700

    Java high concurrency programming design - chap 5 - MultiThread Echo Server and Client test. Verified in command line - need to remove package definition first ...

---
+++ src/main/java/books/javahighconcurrancyprogrammingdesign/chap5_parallelmodeandalgorithm/socketclientserverbasic/Solution.java
@@ -0,0 +1,11 @@
+package books.javahighconcurrancyprogrammingdesign.chap5_parallelmodeandalgorithm.socketclientserverbasic;
+
+/**
+ * Author: blueaken
+ * Date: 5/25/16 12:37
+ */
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("12345");
+    }
+}
