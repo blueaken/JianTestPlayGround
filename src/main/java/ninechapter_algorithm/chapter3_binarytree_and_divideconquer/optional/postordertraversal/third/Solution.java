2516f194b52560a6b17af34a8b63cadd14be8902
BruceShen <blueaken@gmail.com>
2016-06-26 20:06:06 -0700

    nine chapter 3 binary tree and divide conquer - postorder traversal 3rd

--- src/main/java/ninechapter_algorithm/chapter3_binarytree_and_divideconquer/optional/postordertraversal/third/Solution.java
+++ src/main/java/ninechapter_algorithm/chapter3_binarytree_and_divideconquer/optional/postordertraversal/third/Solution.java
@@ -1,37 +1,35 @@
-package ninechapter_algorithm.chapter3_binarytree_and_divideconquer.optional.postordertraversal.second;
+package ninechapter_algorithm.chapter3_binarytree_and_divideconquer.optional.postordertraversal.third;
 
 import type.TreeNode;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Stack;
 
 /**
  * Author: blueaken
- * Date: 4/15/16 3:01 PM
+ * Date: 6/26/16 20:00
  */
-public class Solution_Interative_Without_Map {
+public class Solution {
     /**
      * @param root: The root of binary tree.
      * @return: Postorder in ArrayList which contains node values.
      */
-    public static ArrayList<Integer> postorderTraversal(TreeNode root) {
+    public ArrayList<Integer> postorderTraversal(TreeNode root) {
         // write your code here
         ArrayList<Integer> result = new ArrayList<>();
-        if (root ==  null) {
+        if (root == null) {
             return result;
         }
 
         Stack<TreeNode> stack = new Stack<>();
         stack.push(root);
         TreeNode previous = null;
-        while (stack.size() > 0) {
+        while(stack.size() > 0) {
             TreeNode node = stack.peek();
-            if (node.left == null && node.right == null
+            //如果当前结点没有孩子结点或者孩子节点都已被访问过
+            //正常情况下应该previous ＝ node.right，但在没有右子树的情况下左子树被访问过后也能开始处理当中Node
+            if ((node.left == null && node.right == null)
                     || previous != null && (previous == node.right || previous == node.left)) {
-                //如果当前结点没有孩子结点或者孩子节点都已被访问过
-                //正常情况下应该previous ＝ node.right，但在没有右子树的情况下左子树被访问过后也能开始处理当中Node
                 result.add(node.val);
                 stack.pop();
                 previous = node;
@@ -49,18 +47,20 @@ public class Solution_Interative_Without_Map {
     }
 
     public static void main(String[] args) {
+        Solution solution = new Solution();
+
         //expect 231
-//        TreeNode node = new TreeNode(1);
-//        node.left = new TreeNode(2);
-//        node.right = new TreeNode(3);
+        TreeNode node = new TreeNode(1);
+        node.left = new TreeNode(2);
+        node.right = new TreeNode(3);
 
         //expect 5 13 17 15 10
-        TreeNode node = new TreeNode(10);
-        node.left = new TreeNode(5);
-        node.right = new TreeNode(15);
-        node.right.left = new TreeNode(13);
-        node.right.right = new TreeNode(17);
+//        TreeNode node = new TreeNode(10);
+//        node.left = new TreeNode(5);
+//        node.right = new TreeNode(15);
+//        node.right.left = new TreeNode(13);
+//        node.right.right = new TreeNode(17);
 
-        System.out.println(postorderTraversal(node));
+        System.out.println(solution.postorderTraversal(node));
     }
 }
