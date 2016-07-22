e856cf396a7193fe490852b4dde9a1c944c43510
BruceShen <blueaken@gmail.com>
2016-07-22 11:18:41 -0700

    leetcode shell script - update template and notes

--- src/main/java/leetcode/shellscript/exe.sh
+++ src/main/java/leetcode/shellscript/exe.sh
@@ -1,37 +1,12 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash -vx
 #
-# While Loop Practice 1
+# Debugging Practice 2
 
 # Main body of the shell script starts here.
-while true
-do
-    read -p "
-             1. Show disk usage.
-             2. Show system uptime.
-             3. Show the users logged into the system.
-             What would you like to do? (Enter q to quit.) " CHOICE
-    case $CHOICE in
-        1)
-            df -h
-            echo
-            ;;
-        2)
-            uptime
-            echo
-            ;;
-        3)
-            who
-            echo
-            ;;
-        q)
-            echo "Goodbye!"
-            exit 0
-            ;;
-        *)
-            echo "Invalid Option."
-            ;;
-    esac
-done
+
+ls /etc/passwd
+ls /not/existing
+ls /etc/passwd
 
 # Exit with an explicit exit status.
 exit 0
