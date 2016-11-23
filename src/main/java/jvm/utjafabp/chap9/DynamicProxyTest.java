579fc13cf6018ab9ef7bc8731c1feb7a3c00b969
jianshen <jianshen@ebay.com>
2016-11-22 18:43:22 -0800

    create JVM folder and Dynamic Proxy example

---
+++ src/main/java/jvm/utjafabp/chap9/DynamicProxyTest.java
@@ -0,0 +1,42 @@
+package jvm.utjafabp.chap9;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+/**
+ * Created by jianshen on 11/14/16.
+ */
+public class DynamicProxyTest {
+
+    interface IHello {
+        void sayHello();
+    }
+
+    static class Hello implements IHello  {
+        @Override
+        public void sayHello() {
+            System.out.println("hello world");
+        }
+    }
+
+    static class DynamicProxy implements InvocationHandler {
+        Object originalObj;
+
+        Object bind(Object originalObj) {
+            this.originalObj = originalObj;
+            return Proxy.newProxyInstance(originalObj.getClass().getClassLoader(), originalObj.getClass().getInterfaces(), this);
+        }
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            System.out.println("welcome");
+            return method.invoke(originalObj, args);
+        }
+    }
+
+    public static void main(String[] args) {
+        IHello hello = (IHello) new DynamicProxy().bind(new Hello());
+        hello.sayHello();
+    }
+}
