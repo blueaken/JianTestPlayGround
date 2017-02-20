0428b49d721a719d0b2f758a13816387a6269193
jianshen <jianshen@ebay.com>
2017-02-19 19:40:24 -0800

    add ContextClassLoader (Thread) Test code

--- src/main/java/jvm/classloader/ContextClassLoaderTest.java
+++ src/main/java/jvm/classloader/ContextClassLoaderTest.java
@@ -7,11 +7,15 @@ import java.util.ServiceLoader;
 
 /**
  * Created by jianshen on 2/18/17.
+ *
+ * Ref - http://blog.leanote.com/post/medusar/Java%E7%B1%BB%E5%8A%A0%E8%BD%BD%E6%9C%BA%E5%88%B6
+ *
  */
 public class ContextClassLoaderTest {
 
     public static void main(String[] args) {
-        ServiceLoader<Driver> loader = ServiceLoader.load(Driver.class);
+        ServiceLoader<Driver> loader = ServiceLoader.load(Driver.class, ClassLoader.getSystemClassLoader().getParent());
+//        ServiceLoader<Driver> loader = ServiceLoader.load(Driver.class);
         Iterator<Driver> iterator = loader.iterator();
         while (iterator.hasNext()) {
             Driver driver = (Driver) iterator.next();
