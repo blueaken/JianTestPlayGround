4defae82619b5df0eefe6c44cd5b02c44fa8723e
bruceshen <bruceshen@sh-silverlining.com>
2021-10-12 16:42:50 +0800

    Bubble Sort for Integer Array 463

---
+++ src/main/java/lintcode/colleciton/selected/phase3_array/SortIntegerArray_Bubble_463.java
@@ -0,0 +1,30 @@
+package lintcode.colleciton.selected.phase3_array;
+
+import java.util.Arrays;
+
+public class SortIntegerArray_Bubble_463 {
+    /**
+     * @param A: an integer array
+     * @return: nothing
+     */
+    public static void sortIntegers(int[] A) {
+        // write your code here
+        //control how many rounds
+        for (int i = 1; i < A.length; i++) {
+            //control how many compare times each round
+            for (int j = 0; j < A.length - i; j++) {
+                if (A[j] > A[j+1]) {
+                    int temp = A[j+1];
+                    A[j+1] = A[j];
+                    A[j] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int input[] = {3,2,1};
+        sortIntegers(input);
+        System.out.println(Arrays.toString(input));
+    }
+}
