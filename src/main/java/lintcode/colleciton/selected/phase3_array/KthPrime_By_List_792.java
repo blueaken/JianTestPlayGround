c1865968ba596c147671e001411c1b66cb362f26
bruceshen <bruceshen@sh-silverlining.com>
2021-11-25 17:28:06 +0800

    Kth Prime 792

---
+++ src/main/java/lintcode/colleciton/selected/phase3_array/KthPrime_By_List_792.java
@@ -0,0 +1,45 @@
+package lintcode.colleciton.selected.phase3_array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class KthPrime_By_List_792 {
+    /**
+     * @param n: the number
+     * @return: the rank of the number
+     * ref: https://blog.csdn.net/NK_test/article/details/46242401
+     */
+    public static int kthPrime(int n) {
+        // write your code here
+        // 1. list all primes
+        boolean primes[] = new boolean[100009];
+        // init all positions to true
+        for (int i = 2; i <= n; i++) {
+            primes[i] = true;
+        }
+
+        for (int i = 2; i <= n; i++) {
+            if (primes[i] == true) {
+                for (int j = 2 * i; j <= n; j += i) {
+                    primes[j] = false;
+                }
+            }
+        }
+
+        // find the position
+        int count = 0;
+        for (int i = 2; i <= n; i++) {
+            if (primes[i]) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+//        int input = 23;
+        int input = 63527;
+        System.out.println(kthPrime(input));
+    }
+}
