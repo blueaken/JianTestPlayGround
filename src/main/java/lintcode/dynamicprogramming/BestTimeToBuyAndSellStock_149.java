19ae2b533e73bea8b65a20663a6703b73627d7ed
bruceshen <bruceshen@sh-silverlining.com>
2022-02-27 23:03:03 +0800

    BestTimeToBuyAndSellStock 149

---
+++ src/main/java/lintcode/dynamicprogramming/BestTimeToBuyAndSellStock_149.java
@@ -0,0 +1,24 @@
+package lintcode.dynamicprogramming;
+
+public class BestTimeToBuyAndSellStock_149 {
+    /**
+     * @param prices: Given an integer array
+     * @return: Maximum profit
+     */
+    //Idea: 不断最低价格，比较利润，比较直观；另外，也可以用DP来解。
+    public int maxProfit(int[] prices) {
+        // write your code here
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+
+        int lowestPrice = Integer.MAX_VALUE;
+        int ans = 0;
+        for (int i = 0; i < prices.length; i++) {
+            lowestPrice = Math.min(prices[i], lowestPrice);
+            ans = Math.max(prices[i] - lowestPrice, ans);
+        }
+
+        return ans;
+    }
+}
