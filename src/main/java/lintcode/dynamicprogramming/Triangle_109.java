c1b9126d84db5f71b29f785195991a4f6f3f7b0b
bruceshen <bruceshen@sh-silverlining.com>
2022-03-12 10:19:30 +0800

    Triangle 109

---
+++ src/main/java/lintcode/dynamicprogramming/Triangle_109.java
@@ -0,0 +1,51 @@
+package lintcode.dynamicprogramming;
+
+public class Triangle_109 {
+    /**
+     * @param triangle: a list of lists of integers
+     * @return: An integer, minimum path sum
+     */
+    public int minimumTotal(int[][] triangle) {
+        // write your code here
+        if (triangle == null || triangle[0] == null) {
+            return 0;
+        }
+
+        int row = triangle.length;
+        int col = triangle[row-1].length;
+        int res[][] = new int[row][col];
+
+        //init
+        res[0][0] = triangle[0][0];
+        for (int i = 1; i < row; i++) {
+            res[i][0] = res[i-1][0] + triangle[i][0];
+        }
+
+        //dp
+        for (int i = 1; i < row; i++) {
+            int curCol = triangle[i].length;
+            for (int j = 1; j < curCol; j++) {
+                if (j < curCol - 1) {
+                    res[i][j] = Math.min(res[i-1][j], res[i-1][j-1]) + triangle[i][j];
+                } else {
+                    res[i][j] = res[i-1][j-1] + triangle[i][j];
+                }
+
+            }
+        }
+
+        int minTotal = Integer.MAX_VALUE;
+        for (int i = 0; i < col; i++) {
+            minTotal = Math.min(minTotal, res[row-1][i]);
+        }
+
+        return minTotal;
+    }
+
+    public static void main(String[] args) {
+        int[][] input = {{1},{3,4},{6,5,7},{4,1,8,3}};
+
+        Triangle_109 solution = new Triangle_109();
+        System.out.println(solution.minimumTotal(input));
+    }
+}
