bccbed304c5ce5ccb2c798427deb0668eb7c8e0e
bruceshen <bruceshen@sh-silverlining.com>
2022-04-11 16:56:19 +0800

    ClimbStairs 111 REC

--- src/main/java/lintcode/dynamicprogramming/ClimbStairs_111_REC.java
+++ src/main/java/lintcode/dynamicprogramming/ClimbStairs_111_REC.java
@@ -1,31 +1,25 @@
 package lintcode.dynamicprogramming;
 
-public class ClimbStairs_111 {
+public class ClimbStairs_111_REC {
     /**
      * @param n: An integer
      * @return: An integer
      */
     //Idea: 简单的DP，到达第n阶的方案数为到第n-1阶和n-2阶方案数的总和;
-    //      Ref一下https://www.lintcode.com/problem/111/solution
+    //Ref - https://github.com/mission-peace/interview/blob/master/src/com/interview/dynamic/FibonacciSeries.java
     public int climbStairs(int n) {
         // write your code here
-        if (n == 0) {
-            return 0;
+        if (n == 1 || n == 0) {
+            return 1;
         }
 
-        int[] dp = new int[n+1];
-        dp[0] = 1;
-        dp[1] = 1;
-        for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
-        }
-        return dp[n];
+        return climbStairs(n-1) + climbStairs(n-2);
     }
 
     public static void main(String[] args) {
         int input = 6;
 
-        ClimbStairs_111 solution = new ClimbStairs_111();
+        ClimbStairs_111_REC solution = new ClimbStairs_111_REC();
         System.out.println(solution.climbStairs(input));
     }
 }
