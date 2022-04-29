0ae0b2b30d5818f6c714fd24495357f206ad63b8
bruceshen <bruceshen@sh-silverlining.com>
2022-04-29 15:04:36 +0800

    RangeSumQuery2DMatrix 665

--- src/main/java/lintcode/prefixsum/RangeSumQuery2DMatrix_665.java
+++ src/main/java/lintcode/prefixsum/RangeSumQuery2DMatrix_665.java
@@ -1,23 +1,24 @@
 package lintcode.prefixsum;
 
 public class RangeSumQuery2DMatrix_665 {
-    private int[][] matrix;
+    private int[][] sum;
 
     public RangeSumQuery2DMatrix_665(int[][] matrix) {
-        this.matrix = matrix;
-    }
-
-    public int sumRegion(int row1, int col1, int row2, int col2) {
-
-        //prefix sum
+        //init prefix sum
         int sum[][] = new int[matrix.length + 1][matrix[0].length + 1];
         for (int i = 1; i <sum.length; i++) {
             for (int j = 1; j < sum[0].length; j++) {
                 sum[i][j] = matrix[i-1][j-1] + sum[i-1][j] + sum[i][j-1] - sum[i-1][j-1];
             }
         }
+        this.sum = sum;
+    }
 
-        return sum[row2+1][col2+1] - sum[row1][col2+1] - sum[row2+1][col1] + sum[row1][col1];
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        //use matrix sum base
+        row1++; col1++; row2++; col2++;
+
+        return sum[row2][col2] - sum[row1-1][col2] - sum[row2][col1-1] + sum[row1-1][col1-1];
     }
 
     public static void main(String[] args) {
