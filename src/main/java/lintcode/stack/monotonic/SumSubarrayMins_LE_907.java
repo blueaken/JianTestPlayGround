825a78c15c94a0ce8765c047576e5ead132a806b
bruceshen <bruceshen@sh-silverlining.com>
2022-05-15 17:13:44 +0800

    SumSubarrayMins_LE_907

---
+++ src/main/java/lintcode/stack/monotonic/SumSubarrayMins_LE_907.java
@@ -0,0 +1,44 @@
+package lintcode.stack.monotonic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SumSubarrayMins_LE_907 {
+    /*
+        Idea: use the lps dp template to found the list of sub arrays, then sum the mins
+        Time - O(N^2), not AC TLE
+    */
+    public int sumSubarrayMins(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return 0;
+        }
+        int size = arr.length;
+
+        List<List<Integer>> subarrays = new ArrayList<>();
+        for (int len = 1; len <= size; len ++) {
+            for (int i = 0; i <= size - len; i++) {
+                int j = i + len - 1;
+                List<Integer> sub = new ArrayList<>();
+                int start = i, end = j;
+                while (start <= end) {
+                    sub.add(arr[start++]);
+                }
+                subarrays.add(sub);
+            }
+        }
+
+        int sum = 0;
+        for (List<Integer> sub : subarrays) {
+            sum += getMin(sub);
+        }
+        return sum;
+    }
+
+    private int getMin(List<Integer> sub) {
+        int ans = Integer.MAX_VALUE;
+        for (Integer i : sub) {
+            ans = Math.min(ans, i);
+        }
+        return ans;
+    }
+}
