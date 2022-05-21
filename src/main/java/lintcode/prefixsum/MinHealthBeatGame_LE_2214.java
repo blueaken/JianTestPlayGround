e36364a4fc75af24e86234717391359f78ad88f5
bruceshen <bruceshen@sh-silverlining.com>
2022-05-21 17:36:44 +0800

    MinHealthBeatGame_LE_2214

---
+++ src/main/java/lintcode/prefixsum/MinHealthBeatGame_LE_2214.java
@@ -0,0 +1,19 @@
+package lintcode.prefixsum;
+
+public class MinHealthBeatGame_LE_2214 {
+    public long minimumHealth(int[] damage, int armor) {
+
+        //add all the damages
+        long health = 1;
+        long max = 0;
+        for (int i = 0; i < damage.length; i++) {
+            health += damage[i];
+            max = Math.max(max, damage[i]);
+        }
+
+        //armor need to use at the max damage level for best result
+        health -= Math.min(max, armor);
+
+        return health;
+    }
+}
