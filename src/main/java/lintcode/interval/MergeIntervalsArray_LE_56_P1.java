eab69d751b1a0734d97d0b26462fd8e180d81195
bruceshen <bruceshen@sh-silverlining.com>
2022-06-07 11:01:52 +0800

    MergeIntervalsArray_LE_56_P1

---
+++ src/main/java/lintcode/interval/MergeIntervalsArray_LE_56_P1.java
@@ -0,0 +1,31 @@
+package lintcode.interval;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class MergeIntervalsArray_LE_56_P1 {
+    /*
+        Time - O(NLogN), Space - O(N)
+    */
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        LinkedList<int[]> res = new LinkedList<>();
+        res.add(intervals[0]);
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] > res.getLast()[1]) {
+                res.add(intervals[i]);
+            } else {
+                res.getLast()[1] = Math.max(res.getLast()[1], intervals[i][1]);
+            }
+        }
+        return res.toArray(new int[res.size()][]);
+    }
+
+    public static void main(String[] args) {
+        MergeIntervalsArray_LE_56_P1 solution = new MergeIntervalsArray_LE_56_P1();
+//        int[][] intervals = {{1,3}, {2,6}, {8,10},{15,18}}; //[[1, 6], [8, 10], [15, 18]]
+//        int[][] intervals = {{1,4}, {0,2}, {3,5}}; //[[0, 5]]
+        int[][] intervals = {{2,3},{4,5},{6,7},{8,9},{1,10}}; // [[1,10]]
+        System.out.println(Arrays.deepToString(solution.merge(intervals)));
+    }
+}
