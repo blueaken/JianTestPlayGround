fe339bc3f0aed21b61559088a7f6a7ee3c32417b
bruceshen <bruceshen@sh-silverlining.com>
2022-06-13 17:58:18 +0800

    RectangleOverlap_LE_836

---
+++ src/main/java/lintcode/math/RectangleOverlap_LE_836.java
@@ -0,0 +1,17 @@
+package lintcode.math;
+
+public class RectangleOverlap_LE_836 {
+    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+        //if rec1's right border < rec2's left border and vice verse
+        if (rec1[2] <= rec2[0] || rec2[2] <= rec1[0]) {
+            return false;
+        }
+
+        //if rec1's upper border < rec2's bottom border and vice verse
+        if (rec1[3] <= rec2[1] || rec2[3] <= rec1[1]) {
+            return false;
+        }
+
+        return true;
+    }
+}
