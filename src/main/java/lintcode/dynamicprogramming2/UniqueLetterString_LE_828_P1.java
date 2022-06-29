ae66f6d4ebf9de5b9b32e8f3f8b038a846d0bb4c
bruceshen <bruceshen@sh-silverlining.com>
2022-06-29 17:21:14 +0800

    UniqueLetterString_LE_828_P1

--- src/main/java/lintcode/dynamicprogramming2/UniqueLetterString_LE_828_P1.java
+++ src/main/java/lintcode/dynamicprogramming2/UniqueLetterString_LE_828_P1.java
@@ -1,6 +1,6 @@
 package lintcode.dynamicprogramming2;
 
-public class UniqueLetterString_LE_828 {
+public class UniqueLetterString_LE_828_P1 {
     /*
         - gut feeling is brute force search, Time is O(N^2), N is the length of string
         - it TLEs with no surprise
@@ -8,37 +8,35 @@ public class UniqueLetterString_LE_828 {
 
         - reading the solution link of brilliant O(N) solution, but it does not optimize countUniqueChars(), have to read it triple times to understand
         - ref: https://leetcode.com/problems/count-unique-characters-of-all-substrings-of-a-given-string/discuss/129021/O(N)-Java-Solution-DP-Clear-and-easy-to-Understand
-
     */
     public int uniqueLetterString(String s) {
         if (s == null || s.length() == 0) {
             return 0;
         }
 
-        int[] lastPositionContribution = new int[26];
         int[] contribution = new int[26];
-
-        int res = 0;
+        int[] lastContribution = new int[26];
+        int ans = 0;
         for (int i = 0; i < s.length(); i++) {
-            char cur = s.charAt(i);
+            int cur = s.charAt(i) - 'A';
 
-            //note the string only consists of uppercase letters
-            contribution[cur - 'A'] = i + 1 - lastPositionContribution[cur - 'A'];
-            //other letter's contribution actually unchanged, but it is easy to understand to iterate each letter and get the updated contribution value
-            int current_contribution = 0;
+            int totalNumOfSubStringEndingHere = i + 1;
+            contribution[cur] = totalNumOfSubStringEndingHere - lastContribution[cur];
+
+            int curCharContribution = 0;
             for (int j = 0; j < 26; j++) {
-                current_contribution += contribution[j];
+                curCharContribution += contribution[j];
             }
-            res += current_contribution;
-            //at last update the last position contribution value in case the current letter shows up in the string again
-            lastPositionContribution[cur - 'A'] = i + 1;
-        }
 
-        return res;
+            ans += curCharContribution;
+
+            lastContribution[cur] = i + 1;
+        }
+        return ans;
     }
 
     public static void main(String[] args) {
-        UniqueLetterString_LE_828 solution = new UniqueLetterString_LE_828();
+        UniqueLetterString_LE_828_P1 solution = new UniqueLetterString_LE_828_P1();
         String S = "ABCBD";//27
         System.out.println(solution.uniqueLetterString(S));
     }
