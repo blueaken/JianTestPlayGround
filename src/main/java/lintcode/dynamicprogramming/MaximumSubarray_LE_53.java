531ada06586e9e67d0bfdf03cad20c6726ab32d4
bruceshen <bruceshen@sh-silverlining.com>
2022-08-08 10:27:16 -0700

    MaximumSubarray_LE_53

---
+++ src/main/java/lintcode/dynamicprogramming/MaximumSubarray_LE_53.java
@@ -0,0 +1,17 @@
+package lintcode.dynamicprogramming;
+
+public class MaximumSubarray_LE_53 {
+    /*
+        - ref https://zxi.mytechroad.com/blog/dynamic-programming/leetcode-53-maximum-subarray/
+        - Space can optimize to O(1)
+    */
+    public int maxSubArray(int[] nums) {
+        int ans = nums[0];
+        int sum = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            sum = Math.max(sum + nums[i], nums[i]);
+            ans = Math.max(ans, sum);
+        }
+        return ans;
+    }
+}
