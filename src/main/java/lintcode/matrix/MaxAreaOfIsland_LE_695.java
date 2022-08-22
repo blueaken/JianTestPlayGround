b989cbc1e1588a63ab806a3625069d39889c7a2b
bruceshen <bruceshen@sh-silverlining.com>
2022-08-22 11:54:25 -0700

    MaxAreaOfIsland_LE_695

---
+++ src/main/java/lintcode/matrix/MaxAreaOfIsland_LE_695.java
@@ -0,0 +1,33 @@
+package lintcode.matrix;
+
+public class MaxAreaOfIsland_LE_695 {
+    /*
+        - https://zxi.mytechroad.com/blog/graph/leetcode-695-max-area-of-island/
+        - looks a simple dfs
+    */
+    public int maxAreaOfIsland(int[][] grid) {
+        int max = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                max = Math.max(max, dfs(grid, i, j));
+            }
+        }
+        return max;
+    }
+
+    private int dfs(int[][] grid, int i, int j) {
+        if (i < 0 || i == grid.length || j < 0 || j == grid[0].length || grid[i][j] == 0) {
+            return 0;
+        }
+
+        grid[i][j] = 0;
+
+        return dfs(grid, i + 1, j) + dfs(grid, i, j + 1) + dfs(grid, i - 1, j) + dfs(grid, i, j - 1) + 1;
+    }
+
+    public static void main(String[] args) {
+        MaxAreaOfIsland_LE_695 solution = new MaxAreaOfIsland_LE_695();
+        int[][] grid = {{0,0,1,0,0,0,0,1,0,0,0,0,0},{0,0,0,0,0,0,0,1,1,1,0,0,0},{0,1,1,0,1,0,0,0,0,0,0,0,0},{0,1,0,0,1,1,0,0,1,0,1,0,0},{0,1,0,0,1,1,0,0,1,1,1,0,0},{0,0,0,0,0,0,0,0,0,0,1,0,0},{0,0,0,0,0,0,0,1,1,1,0,0,0},{0,0,0,0,0,0,0,1,1,0,0,0,0}};
+        System.out.println(solution.maxAreaOfIsland(grid));
+    }
+}
