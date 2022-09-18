b9b68ee844ccb3b6c9734860350ca3e8a288a3be
bruceshen <bruceshen@sh-silverlining.com>
2022-09-18 14:51:54 +0800

    MinimumSpaceWastedFromPackaging_LE_1889_OPT

--- src/main/java/lintcode/math/SellDiminishingValuedColoredBalls_LE_1648_Opt_P1.java
+++ src/main/java/lintcode/math/SellDiminishingValuedColoredBalls_LE_1648_Opt_P1.java
@@ -9,9 +9,16 @@ public class SellDiminishingValuedColoredBalls_LE_1648_Opt_P1 {
     - 等差数列求和公式: (首项 + 末项) * 项数 / 2
     - many corner case to consider
     =================
+    example -
+    6 6 6 3 1
+    3 3 3 3 1  - 0 + (6+5+4) * 3
+    1 1 1 1 1  - 45 + (3+2) * 3
+    0 0 0 0 0  - 65 + (1) * 5 = 70
+    =================
      gut feeling - greedy with the help of max heap, Time - O(NlogM), N - order number, M - color kinds
         - get TLE, since color kinds is of 10^9, so the solution should optimize on this
-        - Ref 花花酱讲解 https://www.youtube.com/watch?v=kBxqAnWo9Wo，使用等差数列公式辅助提高性能, Time - O(M)
+        - Ref 花花酱讲解 https://www.youtube.com/watch?v=kBxqAnWo9Wo，使用等差数列公式辅助提高性能,
+        - Time - O(NLogN) + O(1) = O(NLogN) - sorting
         - It is tricky, all test cases passed but still got TLE, try use array instead of Heap to improve performance
         - Java cannot sort reverse order on primitive type array, like int[], need to access the max element from the end
     =================
@@ -39,7 +46,7 @@ public class SellDiminishingValuedColoredBalls_LE_1648_Opt_P1 {
 
             if (color * tran_n <= orders) {
                 sum = (cur + nxt + 1) * tran_n / 2 * color % mod;
-            } else {
+            } else { //ex, cur = 6, nxt = 4; orders = 8, color = 3
                 tran_n = orders / color;
                 int r = orders % color;
                 long nxt_p = cur - tran_n;
