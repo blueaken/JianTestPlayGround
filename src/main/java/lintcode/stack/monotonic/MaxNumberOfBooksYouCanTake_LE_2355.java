c5cb80c950ff483665504691b6dd9bd039c2069d
bruceshen <bruceshen@sh-silverlining.com>
2022-10-11 15:22:35 +0800

    MaxNumberOfBooksYouCanTake_LE_2355

--- src/main/java/lintcode/stack/monotonic/MaxNumberOfBooksYouCanTake_LE_2355.java
+++ src/main/java/lintcode/stack/monotonic/MaxNumberOfBooksYouCanTake_LE_2355.java
@@ -1,43 +1,48 @@
 package lintcode.stack.monotonic;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.Stack;
 
 public class MaxNumberOfBooksYouCanTake_LE_2355 {
     /*
-        ref https://leetcode.com/problems/maximum-number-of-books-you-can-take/discuss/2358714/Java-or-Stack-%2B-DP-or-Find-That-Bottleneck-or-Explanations-and-Proof-or-Comments-while-I-was-coding
-        and make some modificaiton, but fails on test 81/84
+        还是Huifeng Guan讲得清楚 - https://www.youtube.com/watch?v=iJsbDfza-qk
+        dp[i] - the max number of books you can take from an subarry ending at i, now the question is where is the left boundary
+        X X X [ X 3 9 9 9] X
+                  j     i  k
+            dp[j] + 7 8 9
+
+         dp[i] = dp[j] + sum(等差数列)
+         等差数列长度 L = i - j
+         和 = (首项 + 末项) * L / 2 = (books[i] + books[i] - L + 1) * L / 2
+       ---------------------------------------------
+       when k > books[i], dp[k] = dp[i] + books[k];
+       when k < books[i], dp[k] = dp[j] + sum(j+1 : k)
+       --------------------------------------------
+       use monotonic stack for perf, note: use '1L' in front of books[i] when calc 等差数列和 to avoid overflow
     */
     public long maximumBooks(int[] books) {
-        long[] dp = new long[books.length];
-        long maxBooks = 0;
-        Deque<Integer> stack = new ArrayDeque<>();
+        int n = books.length;
+        long[] dp = new long[n];
+        Stack<Integer> stack = new Stack<>();//index
 
-        for (int i = 0; i < books.length; i++){
-            while(!stack.isEmpty() && books[i] < books[stack.peek()]+i-stack.peek()){
+        long ans = 0;
+        for (int i = 0; i < n; i++) {
+            //如果stack top element能和当前元素i组成等差数列，则Pop
+            while (stack.size() > 0 && books[stack.peek()] > (books[i] - (i - stack.peek()))) {
                 stack.pop();
             }
-            int j = stack.isEmpty()? Math.max(0, i-books[i]+1) : stack.peek()+1;
-
-            // dp[i] = 1L*(books[i] * (i-j+1) - (i-j+1) * (i-j) / 2  + (stack.isEmpty()? 0 : dp[j-1]));
-            long sum = getSummation(books[i], i-j+1, -1);
-            dp[i] = sum + (long)(stack.isEmpty()? 0 : dp[j-1]); //fails on test case 81/84
-
-            maxBooks = Math.max(maxBooks, dp[i]);
+            //找到了j的位置，可以用上面公式dp[k] = dp[j] + sum(j+1 : k)计算，注意有2种情况
+            if (stack.size() > 0) {
+                int len = i - stack.peek();
+                dp[i] = dp[stack.peek()] + 1L * (books[i] + books[i] - len + 1) * len / 2;
+            } else {
+                //如果Stack为Empty则整个subarray都可以作为等差数列，len = i + 1, 但注意等差数列首项要大于0
+                //如这种情况：10 10 10 10 10 3，等差数列长度为3，而不是6
+                int len = Math.min(i+1, books[i]);
+                dp[i] = 1L * (books[i] + books[i] - len + 1) * len / 2;
+            }
             stack.push(i);
+            ans = Math.max(ans, dp[i]);
         }
-
-        return maxBooks;
-    }
-
-
-    private long getSummation(int a1, int n, int d) {
-        // formula of sum of book[i] to book[j] is - n*a1+n(n-1)d/2
-        // a1 = books[i]
-        // n = i - j + 1
-        // d = -1
-        // Sn = books[i] * (i-j+1) - (i-j+1) * (i-j) / 2
-
-        return (long)((long)(n*a1)+(long)(n*(n-1)*d)/2);
+        return ans;
     }
 }
