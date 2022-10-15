206efe6a21a478da24585b135030feab6a92831e
bruceshen <bruceshen@sh-silverlining.com>
2022-10-15 18:02:25 +0800

    NumberOfWaysSelectBuildings_LE_2222_DP_P1

---
+++ src/main/java/lintcode/dynamicprogramming2/NumberOfWaysSelectBuildings_LE_2222_DP_P1.java
@@ -0,0 +1,29 @@
+package lintcode.dynamicprogramming2;
+
+public class NumberOfWaysSelectBuildings_LE_2222_DP_P1 {
+    /*
+        ref the solution hint, attack with prefix sum
+        - Time is O(N)
+        ========================================
+        - ref discussion, try a more elegant solution
+        ============================================
+        P1 10.15.2022
+
+    */
+    public long numberOfWays(String s) {
+        long n0 = 0, n1 = 0, n01 = 0, n10 = 0, n010 = 0, n101 = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char cur = s.charAt(i);
+            if (cur == '0') {
+                n0++;
+                n10 += n1;
+                n010 += n01;
+            } else {
+                n1++;
+                n01 += n0;
+                n101 += n10;
+            }
+        }
+        return n010 + n101;
+    }
+}
