f3576987dab447cb104a4f53b950f892ff032882
bruceshen <bruceshen@sh-silverlining.com>
2022-11-10 16:04:19 +0800

    ValidateBST_LE_98_dong

--- src/main/java/lintcode/binarysearchtree/ValidateBST_LE_98_dong.java
+++ src/main/java/lintcode/binarysearchtree/ValidateBST_LE_98_dong.java
@@ -1,8 +1,8 @@
-package lintcode.binarytree;
+package lintcode.binarysearchtree;
 
 import type.TreeNode;
 
-public class ValidateBST_LE_98 {
+public class ValidateBST_LE_98_dong {
     /*
         - ref Huifeng Guan's note
         - 满足BST的三个条件：
@@ -12,33 +12,32 @@ public class ValidateBST_LE_98 {
         在判断第一个条件时，可以根据这个性质：左子树的最大值应该就是左子树最右下角的节点；右子树的最小值应该就是右子树最左下角的节点。
 
         因此很容易写出递归判断的表达式。
+        ========================
+        11.10.2022
+        redo ref 东哥 post
+        ========================
     */
     public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, null, null);
+    }
+
+    /* 限定以 root 为根的子树节点必须满足 max.val > root.val > min.val */
+    private boolean isValidBST(TreeNode root, TreeNode min, TreeNode max) {
         if (root == null) {
             return true;
         }
 
-        TreeNode node;
-        if (root.left != null) {
-            node = root.left;
-            while (node.right != null) {
-                node = node.right;
+        if (min != null) {
+            if (min.val >= root.val) {
+                return false;
             }
-            if (node.val >= root.val) {
+        }
+        if (max != null) {
+            if (max.val <= root.val) {
                 return false;
             }
         }
 
-        if (root.right != null) {
-            node = root.right;
-            while (node.left != null) {
-                node = node.left;
-            }
-            if (node.val <= root.val) {
-                return false;
-            }
-        }
-
-        return isValidBST(root.left) && isValidBST(root.right);
+        return isValidBST(root.left, min, root) && isValidBST(root.right, root, max);
     }
 }
