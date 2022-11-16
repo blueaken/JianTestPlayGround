0ff6766be2ed2d386dcf3d55694bf35ed0931b03
bruceshen <bruceshen@sh-silverlining.com>
2022-11-16 11:51:33 +0800

    MergeKSortedLists_LE_23_P2

--- src/main/java/lintcode/linkedlist/MergeKSortedLists_LE_23_P1.java
+++ src/main/java/lintcode/linkedlist/MergeKSortedLists_LE_23_P1.java
@@ -1,4 +1,4 @@
-package lintcode.heap;
+package lintcode.linkedlist;
 
 import type.ListNode;
 
--- src/main/java/lintcode/linkedlist/MergeKSortedLists_LE_23_P2.java
+++ src/main/java/lintcode/linkedlist/MergeKSortedLists_LE_23_P2.java
@@ -1,35 +1,41 @@
-package lintcode.heap;
-
-import java.util.PriorityQueue;
+package lintcode.linkedlist;
 
 import type.ListNode;
 
-public class MergeKSortedLists_LE_23 {
+import java.util.PriorityQueue;
+
+public class MergeKSortedLists_LE_23_P2 {
     /*
         - ref previous solution, a straightforward way is to use min heap, keep iterate until the heap size is empty
         - Time is O(nlogk), n - is the number of all ListNodes in the final list, k is the number of linked lists, since every poll and offer to the heap takes O(logk).
         - Space is O(n) + O(k), for the result list and priority queue.
+        ===================
+        P2 11.16.2022
+        redo ref 东哥 post
+        ===================
     */
     public ListNode mergeKLists(ListNode[] lists) {
-        if (lists == null || lists.length == 0) {
+        int k = lists.length;
+        if (k == 0) {
             return null;
         }
 
-        PriorityQueue<ListNode> minHeap = new PriorityQueue<>(lists.length, (a, b) -> a.val - b.val);
-        for (ListNode node : lists) {
-            if (node != null) {
-                minHeap.offer(node);
+        PriorityQueue<ListNode> heap = new PriorityQueue<>((a, b) -> a.val - b.val);
+        for (int i = 0; i < k; i++) {
+            if (lists[i] != null) {
+                heap.offer(lists[i]);
             }
         }
 
         ListNode dummy = new ListNode(-1);
         ListNode p = dummy;
-        while (minHeap.size() > 0) {
-            ListNode cur = minHeap.poll();
+        while (heap.size() > 0) {
+            ListNode cur = heap.poll();
             p.next = cur;
             p = p.next;
+
             if (cur.next != null) {
-                minHeap.offer(cur.next);
+                heap.offer(cur.next);
             }
         }
         return dummy.next;
