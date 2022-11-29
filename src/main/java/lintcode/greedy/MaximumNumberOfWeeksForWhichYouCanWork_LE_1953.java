c3397dea5595c600479b0955eb9182878cd12c3d
bruceshen <bruceshen@sh-silverlining.com>
2022-11-29 11:12:10 +0800

    MaximumNumberOfWeeksForWhichYouCanWork_LE_1953

---
+++ src/main/java/lintcode/greedy/MaximumNumberOfWeeksForWhichYouCanWork_LE_1953.java
@@ -0,0 +1,25 @@
+package lintcode.greedy;
+
+public class MaximumNumberOfWeeksForWhichYouCanWork_LE_1953 {
+    /**
+     11.29.2022
+     - a greedy type
+     ref https://leetcode.com/problems/maximum-number-of-weeks-for-which-you-can-work/solutions/1375399/java-solution-o-n-easy-explanation/?languageTags=java
+     */
+    public long numberOfWeeks(int[] milestones) {
+        long sum = 0;
+        int max = 0;
+        int n = milestones.length;
+        for (int i = 0; i < n; i++) {
+            max = Math.max(max, milestones[i]);
+            sum += milestones[i];
+        }
+
+        long rest = sum - max;
+        if (rest >= max - 1) {
+            return sum;
+        } else {
+            return sum - (max - 1 - rest);
+        }
+    }
+}
