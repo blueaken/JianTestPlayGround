90ebcb743e19ec5e250fad8fa7bf85d864baadeb
bruceshen <bruceshen@sh-silverlining.com>
2022-12-16 12:08:04 +0800

    RandomPickWithBlacklist_LE_710

---
+++ src/main/java/lintcode/design/RandomPickWithBlacklist_LE_710.java
@@ -0,0 +1,47 @@
+package lintcode.design;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Random;
+
+public class RandomPickWithBlacklist_LE_710 {
+    /**
+     12.16.2022
+     ref 东哥 post, the idea is put all blacklist into the last range of the array
+     */
+    Random rand;
+    int size;
+    Map<Integer, Integer> map; //black list value, valid index
+    public RandomPickWithBlacklist_LE_710(int n, int[] blacklist) {
+        rand = new Random();
+        size = n - blacklist.length;
+        map = new HashMap<>();
+
+        //mark blacklist values
+        for (Integer black : blacklist) {
+            map.put(black, -1);
+        }
+
+        int last = n - 1; //note - should not be "size - 1", since last should cover the range of the whole array
+        for (Integer black : blacklist) {
+            if (black >= size) {
+                continue;
+            }
+
+            while (map.containsKey(last)) {
+                last--;
+            }
+            map.put(black, last);
+            last--;
+        }
+    }
+
+    public int pick() {
+        int res = rand.nextInt(size);
+        if (!map.containsKey(res)) {
+            return res;
+        } else {
+            return map.get(res);
+        }
+    }
+}
