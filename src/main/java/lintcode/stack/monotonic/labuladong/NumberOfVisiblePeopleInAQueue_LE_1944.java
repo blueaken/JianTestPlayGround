500832e3bbfac883a90b53cb2768e1c46d33cfcb
bruceshen <bruceshen@sh-silverlining.com>
2022-12-27 15:17:42 +0800

    NumberOfVisiblePeopleInAQueue_LE_1944

---
+++ src/main/java/lintcode/stack/monotonic/labuladong/NumberOfVisiblePeopleInAQueue_LE_1944.java
@@ -0,0 +1,32 @@
+package lintcode.stack.monotonic.labuladong;
+
+import java.util.Stack;
+
+public class NumberOfVisiblePeopleInAQueue_LE_1944 {
+    /**
+     12.27.2022
+     - use nge template
+     - count右侧比自己矮的人，并且最后count右侧最高的人如果Stack不为空
+     */
+    public int[] canSeePersonsCount(int[] heights) {
+        Stack<Integer> stack = new Stack<>();
+        int n = heights.length;
+        int[] res = new int[n];
+        //nge template
+        for (int i = n - 1; i >= 0; i--) {
+            int count = 0;
+            while (stack.size() > 0 && stack.peek() < heights[i]) {
+                stack.pop();
+                count++; // ith people can see all people to the righ whose height is less its own height and of monotic increasing
+            }
+
+            // when stack is not empty ith people can see that people whose height is greater than or equal to its height
+            if (stack.size() > 0) {
+                count++;
+            }
+            res[i] = count;
+            stack.push(heights[i]);
+        }
+        return res;
+    }
+}
