4f4ab805f288a69fa4d7d614a5c79389cb66e3e7
bruceshen <bruceshen@sh-silverlining.com>
2023-01-12 23:04:31 +0800

    CountSubIslands_LE_1905

---
+++ src/main/java/lintcode/dfs/island/CountSubIslands_LE_1905.java
@@ -0,0 +1,51 @@
+package lintcode.dfs.island;
+
+public class CountSubIslands_LE_1905 {
+    /**
+     1.12.2023
+     ref 东哥 post，similar to le 1254
+     */
+    public int countSubIslands(int[][] grid1, int[][] grid2) {
+        int m = grid1.length;
+        int n = grid1[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid1[i][j] == 0 && grid2[i][j] == 1) {
+                    //将grid2 中肯定不是sub island的淹没
+                    dfs(grid2, i, j);
+                }
+            }
+        }
+
+        // grid2中剩余的就是sub island了
+        int res = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid2[i][j] == 1) {
+                    res++;
+                    dfs(grid2, i, j);
+                }
+            }
+        }
+        return res;
+    }
+
+    private void dfs(int[][] grid, int i, int j) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        if (i < 0 || j < 0 || i >= m || j >= n) {
+            return;
+        }
+
+        if (grid[i][j] == 0) {
+            return;
+        }
+
+        grid[i][j] = 0;
+        dfs(grid, i+1, j);
+        dfs(grid, i-1, j);
+        dfs(grid, i, j+1);
+        dfs(grid, i, j-1);
+    }
+}
