f318538af09c4bb2b5c6a3f569786dd58029963f
bruceshen <bruceshen@sh-silverlining.com>
2023-01-17 17:09:42 +0800

    CountPrimes_LE_204

---
+++ src/main/java/lintcode/math/prime/CountPrimes_LE_204.java
@@ -0,0 +1,30 @@
+package lintcode.math.prime;
+
+import java.util.Arrays;
+
+public class CountPrimes_LE_204 {
+    /**
+     1.17.2023
+     ref 东哥 post
+     */
+    public int countPrimes(int n) {
+        boolean[] isPrime = new boolean[n];
+        Arrays.fill(isPrime, true);
+
+        for (int i = 2; i * i < n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j < n; j+=i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
