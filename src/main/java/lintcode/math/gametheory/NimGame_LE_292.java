c01a6c078bbf03e53129c6b4ed5ceb77bd13635e
bruceshen <bruceshen@sh-silverlining.com>
2023-01-18 14:27:18 +0800

    NimGame_LE_292

---
+++ src/main/java/lintcode/math/gametheory/NimGame_LE_292.java
@@ -0,0 +1,13 @@
+package lintcode.math.gametheory;
+
+public class NimGame_LE_292 {
+    /**
+     1.18.2023
+     ref 东哥 post
+     */
+    public boolean canWinNim(int n) {
+        // 如果上来就踩到 4 的倍数，那就认输吧
+        // 否则，可以把对方控制在 4 的倍数，必胜
+        return n % 4 != 0;
+    }
+}
