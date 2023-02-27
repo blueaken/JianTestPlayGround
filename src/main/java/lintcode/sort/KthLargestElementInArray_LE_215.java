992241f11f431d2d057cc52a60de184d7d5e60c6
bruceshen <bruceshen@sh-silverlining.com>
2023-02-27 17:54:23 +0800

    KthLargestElementInArray_LE_215

--- src/main/java/lintcode/sort/KthLargestElementInArray_LE_215.java
+++ src/main/java/lintcode/sort/KthLargestElementInArray_LE_215.java
@@ -6,22 +6,28 @@ public class KthLargestElementInArray_LE_215 {
     /**
      11.11.2022
      ref 东哥 post
-     it can be solved with PriorityQueue but it is not challenging with Quick Select Sort
+     it can be solved with PriorityQueue but it is not challenging with Quick Select Sort, O(N)
+     ================
+     1.5.2023
+     try the PriorityQueue solution, O(NlogK)
+     ================
+     2.27.2023
+     try the quick sort + binary search solution, 东哥 template
      */
     public int findKthLargest(int[] nums, int k) {
         // shuffle(nums);
 
-        //将问题转化为第n -k大的元素
+        // 相当于找第n-k个元素, 升序
         k = nums.length - k;
         int lo = 0, hi = nums.length - 1;
         while (lo <= hi) {
             int p = partition(nums, lo, hi);
             if (p < k) {
-                lo = p+1;
+                lo = p + 1;
             } else if (p > k) {
-                hi = p-1;
+                hi = p - 1;
             } else {
-                return nums[p];
+                return nums[k];
             }
         }
         return -1;
@@ -29,37 +35,27 @@ public class KthLargestElementInArray_LE_215 {
 
     private int partition(int[] nums, int lo, int hi) {
         int pivot = nums[lo];
-        int i = lo+1, j = hi;
+        int i = lo + 1, j = hi;
         while (i <= j) {
-            while (i < hi && nums[i] <= pivot) {
+            while (i < nums.length && nums[i] <= pivot) {
                 i++;
             }
-            while (j > lo && nums[j] > pivot) {
+            while (j > 0 && nums[j] > pivot) {
                 j--;
             }
-
             if (i >= j) {
                 break;
             }
-
             swap(nums, i, j);
         }
         swap(nums, lo, j);
         return j;
     }
 
-    private void shuffle(int[] nums) {
-        Random rand = new Random();
-        int n = nums.length;
-        for (int i = 0; i < nums.length; i++) {
-            int r = i + (rand.nextInt(n-i));
-            swap(nums, i, r);
-        }
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 
-    private void swap(int[] nums, int a, int b) {
-        int temp = nums[a];
-        nums[a] = nums[b];
-        nums[b] = temp;
-    }
 }
